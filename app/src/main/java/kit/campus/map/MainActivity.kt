db33eb32d50cf5f111d46c09d0a5f40be679b5dd
Dosugamea <17107514+dosugamea@users.noreply.github.com>
2021-01-16 23:30:03 +0900

    remove: unneeded toolbar

--- app/src/main/java/kit/campus/map/MainActivity.kt
+++ app/src/main/java/kit/campus/map/MainActivity.kt
@@ -37,7 +37,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
